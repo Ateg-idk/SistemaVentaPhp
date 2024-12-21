b863077ae8c9b776dde7413e353aacac14b07b0d
ateg-idk <angelooroanticona@gmail.com>
2024-12-21 10:42:55 -0500

    Ruta dinamica

--- config/app.php
+++ config/app.php
@@ -1,28 +1,38 @@
 <?php
+// Configuración dinámica de APP_URL
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
+$host = $_SERVER['HTTP_HOST'];
+$script_name = dirname($_SERVER['SCRIPT_NAME']);
+$base_path = rtrim($script_name, '/\\') . '/';
 
-	const APP_URL="http://localhost/SistemaVenta/";
-	const APP_NAME="SistemaVenta";
-	const APP_SESSION_NAME="POS";
-
-	/*----------  Tipos de documentos  ----------*/
-	const DOCUMENTOS_USUARIOS=["DNI","Otro"];
-
-
-	/*----------  Tipos de unidades de productos  ----------*/
-	const PRODUCTO_UNIDAD=["Unidad","Caja","Paquete","Lata","Galon","Botella","Bolsa"];
-
-	/*----------  Configuración de moneda  ----------*/
-	const MONEDA_SIMBOLO="S/";
-	const MONEDA_NOMBRE="PEN";
-	const MONEDA_DECIMALES="2";
-	const MONEDA_SEPARADOR_MILLAR=",";
-	const MONEDA_SEPARADOR_DECIMAL=".";
+define("APP_URL", $protocol . "://" . $host . $base_path);
 
 
-	/*----------  Marcador de campos obligatorios (Font Awesome) ----------*/
-	const CAMPO_OBLIGATORIO='&nbsp; <i class="fas fa-edit"></i> &nbsp;';
+// Función para generar rutas
+function url($path = '') {
+    return APP_URL . ltrim($path, '/');
+}
 
-	/*----------  Zona horaria  ----------*/
-	date_default_timezone_set("America/Lima");
+// Configuraciones adicionales
+const APP_NAME = "SistemaVentaPhp";
+const APP_SESSION_NAME = "POS";
 
-	
+// Tipos de documentos
+const DOCUMENTOS_USUARIOS = ["DNI", "Otro"];
+
+// Tipos de unidades de productos
+const PRODUCTO_UNIDAD = ["Unidad", "Caja", "Paquete", "Lata", "Galon", "Botella", "Bolsa"];
+
+// Configuración de moneda
+const MONEDA_SIMBOLO = "S/";
+const MONEDA_NOMBRE = "PEN";
+const MONEDA_DECIMALES = "2";
+const MONEDA_SEPARADOR_MILLAR = ",";
+const MONEDA_SEPARADOR_DECIMAL = ".";
+
+// Marcador de campos obligatorios (Font Awesome)
+const CAMPO_OBLIGATORIO = '&nbsp; <i class="fas fa-edit"></i> &nbsp;';
+
+// Configuración de zona horaria
+date_default_timezone_set("America/Lima");
+?>
